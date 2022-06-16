5e57e34285d6aaf81d4b47249da16ea6b4aa0569
RoBaertschi <rtmbaertschi007@gmail.com>
2022-06-16 16:26:29 +0200

    Hi

--- autoinstall/autoinstall.cpp
+++ autoinstall/autoinstall.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <ostream>
 #include <string>
+#include <vector>
 
 void showUsage(std::string name) {
     std::cerr << "Usage: " << name << " <option(s)> DIRECTORY\n"
@@ -12,6 +13,24 @@ void showUsage(std::string name) {
 int main(int argc, char *argv []) {
 
     showUsage(argv[0]);
+    
+    if (argc < 2) {
+        showUsage(argv[0]);
+        return EXIT_FAILURE;
+    }
+    std::vector<std::string> directory;
+    std::string destination;
+    for (int i = 1; i < argc; ++i) {
+        std::string arg{argv[i]};
+        if ((arg == "-h") || (arg == "--help")) {
+            showUsage(argv[0]);
+            return EXIT_SUCCESS;
+        } else if ((arg == "--build-system") || (arg == "-b")) {
+            
+        } else {
+            destination.push_back(argv[i]);
+        }
+    }
 
-    return EXIT_FAILURE;
+    return EXIT_SUCCESS;
 }
